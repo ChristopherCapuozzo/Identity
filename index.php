bdd08e688a600583149630d090c49588ff17ec39
Christopher <capuozzochris@gmail.com>
2022-02-22 10:36:52 +0100

    V.1.0.1

--- index.php
+++ index.php
@@ -8,21 +8,15 @@
     <title>Document</title>
 </head>
 <body>
-    <p class="titre-menu">Mes Variable en PHP</p>
+    <p class="titre-menu">Générateur D'itentité</p>
+    <p class="description">Faite F5 ou CTRL + R pour généré une nouvelle identité</p>
     <div class="box">
         <?php
-
-            /**
-             * New Methode the Get JSON
-             */
-
-            // List the name
             $data_JSON = file_get_contents("nameAll.json"); 
             $decode = json_decode($data_JSON, true);
             $gender_typy;
             $age = rand(5, 75);
-
-            // Query input
+            
             $random = shuffle($decode);
             $get_input = $decode[$random];
 
@@ -35,35 +29,6 @@
             echo 'Age : ', $age;
             echo '<br>';
             echo 'Majeur : ', $age > 17 ? "Oui" : "Non";
-
-
-            //Prénom
-           /* $prenom_M = array("Pierre", "Nicolas", "Jean Pascal", "Loris", "Guiotin", "Hugo", "Sacha", "Christopher");
-            $prenom_F = array("Elise", "Ambre", "Sacha", "Natacha", "Thyphany", "Zina", "Lousia", "Sophie");
-            $prenomAll = array_merge($prenom_F,$prenom_M);
-            $value_name_test = $prenomAll[shuffle($prenomAll)]; 
-            $gender_boolean;
-
-            //Âge
-            $age = rand(5, 75);
-             
-            if (in_array($value_name_test, $prenom_M)) {
-                $gender_boolean = "Homme";
-            } elseif (in_array($value_name_test, $prenom_F)) {
-                $gender_boolean = "Femme";
-            } else {
-                $gender_boolean = "Non genre";
-            }
-
-            $majeur = $age > 17 ? "Oui" : "Non"; 
-
-            echo 'Prénom : ', $value_name_test;
-            echo '<br>';
-            echo 'Genre : ', $gender_boolean;
-            echo '<br>';
-            echo 'Age : ', $age;
-            echo '<br>';
-            echo 'Majeur : ', $majeur;*/
             ?>
     </div>
 </body>
