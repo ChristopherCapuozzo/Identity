3a5d5f5f837e8423f6b64fc2e57195608acbfe7b
Christopher <capuozzochris@gmail.com>
2022-02-22 13:30:39 +0100

    V.1.0.2

--- index.php
+++ index.php
@@ -29,6 +29,53 @@
             echo 'Age : ', $age;
             echo '<br>';
             echo 'Majeur : ', $age > 17 ? "Oui" : "Non";
+            echo '<br>';
+            echo '<br>';
+            /*##################################################*/
+            echo '<br>';
+            $majeur = rand(17, 19);
+
+            switch($majeur) {
+                case $majeur > 17;
+                    echo 'Majeur : ', $majeur;
+                    break;
+                default:
+                    echo 'Mineur : ', $majeur;
+            };
+
+            echo '<br>';
+            /*##################################################*/
+            echo 'Suite Logique ';
+
+            $i = 1;
+
+            while ($i <= 5):
+                echo ' > ', $i;
+                $i++;
+            endwhile;
+            
+
+            /*##################################################*/
+
+            $firstCal = rand(1, 100);
+            $secondCal = rand(1, 100);
+
+            $additionCal = $firstCal + $secondCal;
+            $multipliCal = $firstCal * $secondCal;
+            $soustractionCal = $firstCal - $secondCal;
+
+
+            echo '<br>';
+            echo '<br>';
+            echo $firstCal, ' + ', $secondCal, ' = ', $additionCal;
+            echo '<br>';
+            echo $firstCal, ' x ', $secondCal, ' = ', $multipliCal;
+            echo '<br>';
+            echo $firstCal, ' - ', $secondCal, ' = ', $soustractionCal;
+
+
+            
+
             ?>
     </div>
 </body>
